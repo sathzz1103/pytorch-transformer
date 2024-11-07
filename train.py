7cd577c35053040e8d9ec01c8cf1906f10ef1acc
sathzz1103 <sathzz1103@gmail.com>
2024-11-07 15:54:03 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
-from model import build_transformer
+from model import build_transformer, build_student_transformer
 from dataset import BilingualDataset, causal_mask
 from config import get_config, get_weights_file_path, latest_weights_file_path
 
-import torchtext.datasets as datasets
+from datasets import load_dataset
 import torch
 import torch.nn as nn
+import torch.nn.utils.prune as prune
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim.lr_scheduler import LambdaLR
 
@@ -13,8 +14,6 @@ from tqdm import tqdm
 import os
 from pathlib import Path
 
-# Huggingface datasets and tokenizers
-from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
