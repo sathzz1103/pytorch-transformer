37c73b6712d501457a1a1321783ed6e275e69b7d
sathzz1103 <sathzz1103@gmail.com>
2024-11-07 15:59:07 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,10 @@
-from model import build_transformer, build_student_transformer
+from model import build_transformer
 from dataset import BilingualDataset, causal_mask
 from config import get_config, get_weights_file_path, latest_weights_file_path
 
 from datasets import load_dataset
 import torch
 import torch.nn as nn
-import torch.nn.utils.prune as prune
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim.lr_scheduler import LambdaLR
 
